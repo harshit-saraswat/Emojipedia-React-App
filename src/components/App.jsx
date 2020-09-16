cd266917517dec57cdd7393f0bcfe0ff81db932d
Harshit Saraswat <41959164+harshit-saraswat@users.noreply.github.com>
2020-09-17 00:15:20 +0530

    Completed Mapping Challenge for Emojipedia App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,52 +1,25 @@
 import React from "react";
+import Header from "./Header";
+import Item from "./Item";
+import emojipedia from "../emojipedia";
+
+function createCard(emojiData) {
+  return (
+    <Item
+      key={emojiData.id}
+      name={emojiData.name}
+      emoji={emojiData.emoji}
+      meaning={emojiData.meaning}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
+      <Header />
 
-      <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
-      </dl>
+      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
     </div>
   );
 }
