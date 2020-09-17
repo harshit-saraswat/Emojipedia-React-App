8f8f15ff4c7023f045a52499cea3d1cccd367172
Harshit Saraswat <41959164+harshit-saraswat@users.noreply.github.com>
2020-09-18 00:09:01 +0530

    Implemented Arrow Function syntax

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,23 +3,34 @@ import Header from "./Header";
 import Item from "./Item";
 import emojipedia from "../emojipedia";
 
-function createCard(emojiData) {
-  return (
-    <Item
-      key={emojiData.id}
-      name={emojiData.name}
-      emoji={emojiData.emoji}
-      meaning={emojiData.meaning}
-    />
-  );
-}
+// function createCard(emojiData) {
+//   return (
+//     <Item
+//       key={emojiData.id}
+//       name={emojiData.name}
+//       emoji={emojiData.emoji}
+//       meaning={emojiData.meaning}
+//     />
+//   );
+// }
 
 function App() {
   return (
     <div>
       <Header />
 
-      <dl className="dictionary">{emojipedia.map(createCard)}</dl>
+      <dl className="dictionary">
+        {emojipedia.map((emojiData) => {
+          return (
+            <Item
+              key={emojiData.id}
+              name={emojiData.name}
+              emoji={emojiData.emoji}
+              meaning={emojiData.meaning}
+            />
+          );
+        })}
+      </dl>
     </div>
   );
 }
